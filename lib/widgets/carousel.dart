be0c801bffd16bd54452dfe3d5d5f43e757eeb6b
hamiiid <Hamiiid.Asgarian@gmail.com>
2021-08-02 20:30:55 +0430

    data reorganizing

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -27,8 +27,7 @@ class _CarouselSectionState extends State<CarouselSection> {
       PageController(initialPage: 1, keepPage: false, viewportFraction: 0.85);
 
   //NOTE carousel builder function
-  List<Widget> slideBuilder(
-      List list, PageController controller, Map referenceData)
+  List<Widget> slideBuilder(List list, PageController controller)
   //? referenceData is the data gotten to route into  Product page
   {
     final List<Widget> carouselsList = [];
@@ -40,7 +39,7 @@ class _CarouselSectionState extends State<CarouselSection> {
         // hasLabel: element["hasLabel"],
         label: element["label"], //if null, no label is created
         imageUrl: element["imageURL"],
-        data: referenceData[element["ctegory"]],
+        // data: referenceData[element["ctegory"]],
         title: element["ctegory"],
       ));
     });
@@ -85,27 +84,38 @@ class _CarouselSectionState extends State<CarouselSection> {
               // height: 200,
               // color: Colors.pink,
               child: Column(children: [
-            FutureBuilder(
-                future: vals.getDataFromApi(
-                    url:
-                        'http://localhost:3000/scrollableItems'), //NOTE internal data recieve for generating selected items products page
-                builder:
-                    (context, AsyncSnapshot<Map<dynamic, dynamic>> snapshot) {
-                  if (snapshot.hasData) {
-                    return Expanded(
-                      child: PageView(
-                          onPageChanged: (value) {
-                            setState(() {
-                              currentpage = value;
-                            });
-                          },
-                          controller: controller,
-                          children: slideBuilder(
-                              widget.items, controller, snapshot.data!)),
-                    );
-                  }
-                  return CircularProgressIndicator();
-                }),
+            // FutureBuilder(
+            //     future: vals.getDataFromApi(
+            //         url:
+            //             'http://localhost:3000/scrollableItems'), //NOTE internal data recieve for generating selected items products page
+            //     builder:
+            //         (context, AsyncSnapshot<Map<dynamic, dynamic>> snapshot) {
+            //       if (snapshot.hasData) {
+            //         return Expanded(
+            //           child: PageView(
+            //               onPageChanged: (value) {
+            //                 setState(() {
+            //                   currentpage = value;
+            //                 });
+            //               },
+            //               controller: controller,
+            //               children: slideBuilder(
+            //                   widget.items, controller, snapshot.data!)),
+            //         );
+            //       }
+            //       return CircularProgressIndicator();
+            //     }),
+            Expanded(
+              child: PageView(
+                  onPageChanged: (value) {
+                    setState(() {
+                      currentpage = value;
+                    });
+                  },
+                  controller: controller,
+                  children: slideBuilder(widget.items, controller)),
+            ),
+
             Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: counterDotsBuilder(widget.items, currentpage))
@@ -142,73 +152,86 @@ class Slide extends StatefulWidget {
 class _SlideState extends State<Slide> {
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: widget.controller,
-      builder: (context, child) {
-        double _value = 1.0;
-        if (widget.controller.position.haveDimensions) {
-          // print(controller.position.viewportDimension);
-          _value = widget.controller.page! - widget.index;
-          _value = (1 - (_value.abs() * .5)).clamp(0.0, 1.0);
-        } else {
-          _value = widget.controller.initialPage.toDouble() - widget.index;
-          _value = (1 - (_value.abs() * .5)).clamp(0.0, 1.0);
-        }
-        // print(context.size);
-        return Center(
-          child: GestureDetector(
-            onTap: () {
-              //NOTE sending to new items page
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ListedItemsScreen(
-                          title: widget.title, itemsList: widget.data)));
-            },
-            ////////////////////////////////////////////////* slide's main frame
-            child: Stack(
-              children: [
-                Padding(
-                  //NOTE gap between pages
-                  padding: EdgeInsets.symmetric(horizontal: 2.5),
-                  child: Align(
-                    //NOTE slides animating aspect ratio
-                    child: AspectRatio(
-                      aspectRatio: 1.7 / Curves.easeOut.transform(_value),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(5),
-                          child: Image.network(widget.imageUrl ?? "",
-                              fit: BoxFit.cover)),
+    return Consumer<ProviderModel>(builder: (context, vals, child) {
+      return AnimatedBuilder(
+        animation: widget.controller,
+        builder: (context, child) {
+          double _value = 1.0;
+          if (widget.controller.position.haveDimensions) {
+            // print(controller.position.viewportDimension);
+            _value = widget.controller.page! - widget.index;
+            _value = (1 - (_value.abs() * .5)).clamp(0.0, 1.0);
+          } else {
+            _value = widget.controller.initialPage.toDouble() - widget.index;
+            _value = (1 - (_value.abs() * .5)).clamp(0.0, 1.0);
+          }
+          // print(context.size);
+          return Center(
+            child: GestureDetector(
+              onTap: () async {
+                var categoryData = await vals.getDataFromApiToList(
+                    url: 'http://localhost:3000/${widget.title}');
+
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ListedItemsScreen(
+                      title: widget.title, itemsList: categoryData);
+                }));
+              },
+
+              // onTap: () {
+              //   //NOTE sending to new items page
+              //   Navigator.push(
+              //       context,
+              //       MaterialPageRoute(
+              //           builder: (context) => ListedItemsScreen(
+              //               title: widget.title, itemsList: widget.data)));
+              // },
+              ////////////////////////////////////////////////* slide's main frame
+              child: Stack(
+                children: [
+                  Padding(
+                    //NOTE gap between pages
+                    padding: EdgeInsets.symmetric(horizontal: 2.5),
+                    child: Align(
+                      //NOTE slides animating aspect ratio
+                      child: AspectRatio(
+                        aspectRatio: 1.7 / Curves.easeOut.transform(_value),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Image.network(widget.imageUrl ?? "",
+                                fit: BoxFit.cover)),
+                      ),
                     ),
                   ),
-                ),
-                //////////////////////////////////////////////////////// * label
-                if ((widget.label != "") && (widget.label != null))
-                  Align(
-                    alignment: const Alignment(-0.7, 0.6),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Container(
-                        color: Colors.white,
-                        width: 80,
-                        height: 25,
-                        child: Center(
-                          child: Text(
-                            widget.label!, ////////////////////////* label text
-                            style: standardSearchFontStyle,
+                  //////////////////////////////////////////////////////// * label
+                  if ((widget.label != "") && (widget.label != null))
+                    Align(
+                      alignment: const Alignment(-0.7, 0.6),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Container(
+                          color: Colors.white,
+                          width: 80,
+                          height: 25,
+                          child: Center(
+                            child: Text(
+                              widget
+                                  .label!, ////////////////////////* label text
+                              style: standardSearchFontStyle,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  )
-                else
-                  const SizedBox()
-              ],
+                    )
+                  else
+                    const SizedBox()
+                ],
+              ),
             ),
-          ),
-        );
-      },
-      // child:
-    );
+          );
+        },
+        // child:
+      );
+    });
   }
 }
