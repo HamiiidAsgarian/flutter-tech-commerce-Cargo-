6a44c45509e7d97daf2c5822611727f058974803
hamiiid <Hamiiid.Asgarian@gmail.com>
2021-08-10 18:41:21 +0430

    Refactor listed items grid to Lazy Loading

--- lib/screens/99_test_screen.dart
+++ lib/screens/99_test_screen.dart
@@ -1,8 +1,18 @@
-main() {
-  var a = {
-    "a": {1},
-    "s": {2},
-    "aa": {3}
-  };
-  print(a.values.toList()[2]);
-}
+// import 'dart:async';
+
+// main() {
+//   List<int> a = [2, 5, 1, 3, 6, 7, 0, 9];
+//   var b = [];
+//   int big = 0;
+//   for (var i = 0; i < a.length; i++) {
+//     for (var j = 0; j < a.length; j++) {
+//       if (a[j] > a[i]) {
+//         int z = a[j];
+//         a[j] = a[i];
+//         a[i] = z;
+//       }
+//     }
+//   }
+
+//   print(a);
+// }
